a332d82c616383dc852cfabd2c3586b9d2b50631
luischapid <luischapid03@gmail.com>
2025-02-19 15:29:42 -0500

    haciendo flujo para la entrada a safre

--- client/src/view/landingPage/LandingPage.jsx
+++ client/src/view/landingPage/LandingPage.jsx
@@ -1,10 +1,30 @@
+import PropTypes from "prop-types";
 import pizza from "../../assets/image/pizza.jpeg";
 
-function LandingPage() {
-  return <div>
-    LandingPage Soy la landing
-    <img src={pizza} alt="pizza" />
-    </div>;
+function LandingPage({ onEnter }) {
+  return (
+    <div className="relative w-full h-screen">
+      <img
+        src={pizza}
+        alt="pizza"
+        className="absolute w-full h-full object-cover"
+      />
+
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
+        <h1 className="text-4xl font-bold mb-4">Bienvenido</h1>
+        <button
+          className="px-6 py-3 bg-red-500 hover:bg-red-600 rounded-lg text-lg font-semibold"
+          onClick={onEnter}
+        >
+          Entrar
+        </button>
+      </div>
+    </div>
+  );
 }
+
+LandingPage.propTypes = {
+  onEnter: PropTypes.func.isRequired,
+};
 
 export default LandingPage;
