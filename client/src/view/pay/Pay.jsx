41c149dc4b6f45766e08557d8e1d0be3f06f216d
Maryxale <romerobencomo@gmail.com>
2025-02-20 18:53:57 -0300

    colores en tailwind.config,avances en view pay

--- client/src/view/pay/Pay.jsx
+++ client/src/view/pay/Pay.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+import Icono_select from "../../assets/icons/icono_select.svg";
 
 function Pay() {
   const [activeSection, setActiveSection] = useState(null);
+  const [hour, setHour] = useState("");
+  const [daily, setDaily] = useState("");
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
   return (
-    <div className="flex flex-col items-center p-6 text-left text-black bg-gray-900 min-h-screen ">
+    <div className="flex flex-col items-center p-6 text-left text-black bg-blue_grisaceo-50 min-h-screen ">
       {/*Franja superior*/}
       <div className="w-full bg-white p-3 text-lg font-bold">Pagos/Ventas</div>
       {/*Botones*/}
@@ -30,6 +37,111 @@ function Pay() {
           Movimientos de Caja
         </button>
       </div>
+      {/*Selects*/}
+      <div className="w-full bg-blue_grisaceo-50 p-4 rounded-lg flex flex-wrap gap-4 justify-center">
+        <div>
+          <img className="w-10 h-10" src={Icono_select} alt="" />
+        </div>
+        <FormControl size="small" className="w-40">
+          <InputLabel sx={{ color: "black" }}>Hora de Inicio</InputLabel>
+          <Select
+            value={hour}
+            onChange={(e) => setHour(e.target.value)}
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              "& .MuiSelect-icon": { color: "black" },
+            }}
+            label="Hora de Inicio"
+          >
+            <MenuItem value="08:00">08:00</MenuItem>
+            <MenuItem value="12:00">12:00</MenuItem>
+            <MenuItem value="16:00">16:00</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl size="small" className="w-40">
+          <InputLabel sx={{ color: "black" }}>Diario</InputLabel>
+          <Select
+            value={daily}
+            onChange={(e) => setDaily(e.target.value)}
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              "& .MuiSelect-icon": { color: "black" },
+            }}
+            label="Diario"
+          >
+            <MenuItem value="Lunes">Lunes</MenuItem>
+            <MenuItem value="Martes">Martes</MenuItem>
+            <MenuItem value="Miércoles">Miércoles</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl size="small" className="w-40">
+          <InputLabel sx={{ color: "black" }}>Día</InputLabel>
+          <Select
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              "& .MuiSelect-icon": { color: "black" },
+            }}
+            label="Día"
+          >
+            <MenuItem value="1">1</MenuItem>
+            <MenuItem value="15">15</MenuItem>
+            <MenuItem value="30">30</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl size="small" className="w-40">
+          <InputLabel sx={{ color: "black" }}>Mes</InputLabel>
+          <Select
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              "& .MuiSelect-icon": { color: "black" },
+            }}
+            label="Mes"
+          >
+            <MenuItem value="Enero">Enero</MenuItem>
+            <MenuItem value="Febrero">Febrero</MenuItem>
+            <MenuItem value="Marzo">Marzo</MenuItem>
+            <MenuItem value="Abril">Abril</MenuItem>
+            <MenuItem value="Mayo">Mayo</MenuItem>
+            <MenuItem value="Junio">Junio</MenuItem>
+            <MenuItem value="Julio">Julio</MenuItem>
+            <MenuItem value="Agosto">Agosto</MenuItem>
+            <MenuItem value="Septiembre">Septiembre</MenuItem>
+            <MenuItem value="Octubre">Octubre</MenuItem>
+            <MenuItem value="Noviembre">Noviembre</MenuItem>
+            <MenuItem value="Diciembre">Diciembre</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl size="small" className="w-40">
+          <InputLabel sx={{ color: "black" }}>Año</InputLabel>
+          <Select
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              "& .MuiSelect-icon": { color: "black" },
+            }}
+            label="Año"
+          >
+            <MenuItem value="2023">2023</MenuItem>
+            <MenuItem value="2024">2024</MenuItem>
+            <MenuItem value="2025">2025</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
       {/*Contenido Dinamico*/}
       <div className="w-full p-5 bg-gray-800 rounded-lg">
         {activeSection === "ventas" && <div>Contenido de Ventas</div>}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue_grisaceo: {
+          50:"#252836" 
+        }
+      }
+    },
   },
   plugins: [],
 }
