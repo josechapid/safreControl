db0c6ce98de46b92e574b743ccd92d8194325a42
luischapid <luischapid03@gmail.com>
2025-04-07 23:33:02 -0500

    agregando products, stathetics, settings, tables

---
+++ client/src/view/settings/Settings.jsx
@@ -0,0 +1,14 @@
+
+import "./Home.css"
+import loro from "../../assets/image/imagen-vectorial.jpg"
+function Home() {
+  return (
+    <div>
+      <div className="home">
+        <img className="home-image" src={loro} alt="loro" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
