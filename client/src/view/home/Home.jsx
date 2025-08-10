b4e2e68258666b3b35e9ed6f6f81fce8bc0f7037
luischapid <luischapid03@gmail.com>
2025-08-09 21:36:15 -0500

    cambios en la pagina de home, main, aside

--- client/src/view/home/Home.jsx
+++ client/src/view/home/Home.jsx
@@ -1,12 +1,44 @@
-
-import "./Home.css"
-import loro from "../../assets/image/imagen-vectorial.jpg"
+import "./Home.css";
+import loro from "../../assets/image/imagen-vectorial.jpg";
 function Home() {
   return (
     <div>
-      <div className="home">
-        <img className="home-image" src={loro} alt="loro" />
-      </div>
+      <header className="header">
+        <h1 className="title">Mesas</h1>
+      </header>
+      <main className="main-container">
+        <section className="main-section">
+          <img src={loro} alt="" className="home-image" />
+        </section>
+        <aside className="aside-container">
+          <div className="aside-title">
+            <h1>Mesa</h1>
+          </div>
+          <div className="aside-labels">
+            <label htmlFor="waiter">
+              Mesero/a:
+              <select id="waiter" name="waiter">
+                <option value="jose">Jose</option>
+                <option value="andrea">Andrea</option>
+                <option value="omaira">Omaira</option>
+              </select>{" "}
+            </label>
+
+            <label htmlFor="people">
+              No. personas:
+              <select id="people" name="people">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </label>
+          </div>
+          <div className="aside-buttons">
+            <button>Nueva Orden</button>
+            <button>Abrir Mesa</button>
+          </div>
+        </aside>
+      </main>
     </div>
   );
 }
